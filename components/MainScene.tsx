bf0317570bc565671939d13ab0d0dfbedad6e459
Thang Vu <thvu@hey.com>
2021-06-25 14:56:15 +0700

    WIP models

--- components/MainScene.tsx
+++ components/MainScene.tsx
@@ -1,20 +1,26 @@
 import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import React, { Suspense } from "react";
-import CameraModel from "./CameraModel";
+import { Canvas, Props as CanvasProps } from "@react-three/fiber";
+import React, { Suspense, useEffect, useMemo, useState } from "react";
+import BackgroundModel from "./models/Background";
 
 export default function MainScene(): JSX.Element {
+  const [cameraPosition] = useState<CanvasProps["camera"]>({
+    position: [-60, 5, 45],
+  });
+  const canvasStyle = useMemo(() => ({ width: "100vw", height: "100vh" }), []);
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    // animate using gsap
+    setScrolled(true);
+  }, []);
   return (
-    <Canvas
-      camera={{ position: [0, 0, 35] }}
-      style={{ width: "100vw", height: "100vh" }}
-    >
+    <Canvas camera={cameraPosition} style={canvasStyle}>
       <ambientLight intensity={10} />
       <pointLight position={[45, 45, 45]} />
       <Suspense fallback={null}>
-        <CameraModel />
+        <BackgroundModel />
       </Suspense>
-      <OrbitControls />
+      {scrolled && <OrbitControls />}
     </Canvas>
   );
 }
