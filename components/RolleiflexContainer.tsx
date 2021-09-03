2535eee37cc3212f44a1cc60745d68527172d7dc
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2021-09-03 17:19:24 +0700

    fix: timeline in hook (#17)

--- components/RolleiflexContainer.tsx
+++ components/RolleiflexContainer.tsx
@@ -1,5 +1,5 @@
 import { useThree } from "@react-three/fiber";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Rolleiflex from "./models/Rolleiflex";
@@ -8,6 +8,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 const sceneRotation = {
   front: { x: 0, y: 3.1, z: 0 },
+  frontReverse: { x: 0, y: -3.1, z: 0 },
   right: { x: 0, y: 1.7, z: 0 },
   left: { x: 0, y: -1.7, z: 0 },
   top: { x: 1.5, y: 0, z: 0 },
@@ -17,16 +18,10 @@ const cameraPosition = {
   zoomedOut: { x: 0, y: 3, z: 10 },
   zoomedIn: { x: 0, y: 2, z: 6 },
 };
+
 export default function RolleiflexContainer() {
   const { scene, camera } = useThree();
-  const rolleiflexTimeline = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".section-two",
-      endTrigger: ".section-six",
-      end: "top top",
-      scrub: true,
-    },
-  });
+  const rolleiflexTimeline = useRef<gsap.core.Timeline>();
 
   useEffect(() => {
     scene.rotation.set(
@@ -40,7 +35,15 @@ export default function RolleiflexContainer() {
       cameraPosition.zoomedOut.z
     );
 
-    rolleiflexTimeline
+    rolleiflexTimeline.current = gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".section-two",
+          endTrigger: ".section-six",
+          end: "top top",
+          scrub: true,
+        },
+      })
       .to(camera.position, { ...cameraPosition.zoomedIn })
       .to(scene.rotation, { ...sceneRotation.right })
       .to(scene.rotation, { ...sceneRotation.back })
@@ -48,7 +51,7 @@ export default function RolleiflexContainer() {
       .to(scene.rotation, { ...sceneRotation.top })
       .to(camera.position, { ...cameraPosition.zoomedIn })
       .to(scene.rotation, { ...sceneRotation.left })
-      .to(scene.rotation, { y: -3.1 })
+      .to(scene.rotation, { ...sceneRotation.frontReverse })
       .to(camera.position, { ...cameraPosition.zoomedOut });
   }, []);
 
